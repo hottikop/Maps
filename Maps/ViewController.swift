80c7677a0f804b324e4a7a9e08e988e0504285fe
Maksim Tseligorov <imaxik75@gmail.com>
2023-11-24 10:12:02 +0300

    Initial Commit

---
+++ Maps/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by Максим Целигоров on 24.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
